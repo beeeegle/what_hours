282ec07d5e193d45dac25687317e44e3ba1fa216
beeeegle <>
2024-07-03 15:49:11 +0900

    ページタイトル、計算を修正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,9 @@
       <v-row>
         <v-col cols="12">
           <h1>これって何時間？</h1>
+          入力した秒を時間に変換できるサイトです！<br>
+          例えば・・・「86400」と入力したら、「24時間」になり、<br>
+          「1728000」と入力したら、「480時間」になります。
         </v-col>
         <v-col cols="12">
           <h1>{{ sec2Hours(time) }}</h1>
@@ -31,21 +34,18 @@
   const time = ref<number>(0)
 
   const sec2Hours = (seconds: number): string => {
-    const day: number = Math.floor(seconds / 86400)
-    const hour: number = Math.floor(seconds % 86400 / 3600)
+    const hour: number = Math.floor(seconds / 3600)
     const min: number = Math.floor(seconds % 3600 / 60)
     const sec: number = seconds % 60
     let time = ''
-    if(day !== 0) {
-      time = `${day}日${hour}時間${min}分${sec}秒`;
-    }else if(hour !==0 ) {
-      time = `${hour}時間${min}分${sec}秒`;
+    if(hour !==0 ) {
+      time = `${hour}時間${min}分${sec}秒`
     }else if(min !==0) {
-      time = `${min}分${sec}秒`;
+      time = `${min}分${sec}秒`
     }else {
-      time = `${sec}秒`;
+      time = `${sec}秒`
     }
-    return time;
+    return time
   }
 </script>
 <style>
